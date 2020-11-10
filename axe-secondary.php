6d0cbda22ab8cfc7e6a067dd37c5b1cf959ff39c
EsauReis <esau.santos@fatec.sp.gov.br>
2020-11-09 21:22:15 -0300

    Alterações

---
+++ axe-secondary.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="images/favico.ico">
+    <meta name="description" content="Axé ">
+    <meta name="author" content="Felipe Vinicius da Silva">
+ 
+    <title>Axé</title>
+
+
+    <link rel="stylesheet" href="css/normalize.css" />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+       <link href="css/base.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+   <div class="container">
+            <?php
+            include 'includes/nav.php';
+            ?>
+
+    <div class="row">
+        <header class="col-12 col-md-12 col-sm-12">
+            <img src="images/header.jpg" width="100%" height="300" />
+        </header>
+    </div>
+        <div class="row">
+            
+            <header class="col">
+                <h1>Axé</h1>
+                <p class="lead">
+                    Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+                </p>
+                <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas faucibus, eros sit amet vestibulum vestibulum, elit ipsum ornare dui, nec molestie turpis elit nec sapien.</p>
+            </header>
+        </div>
+        <div class="row">
+            <article class="col-lg-6 col-md-6 col-sm-12">
+         
+             <img src="images/claudialeite.jpeg" alt="claudia leite em cima de um trio-eletreico em um tom cinza" width="300" height="300">
+             <img src="images/ivetesangalo.jpeg" alt="Ivete Sangalo em um show , a frente a esta a Ivete e atras o público em roxo" width="300" height="300">
+             <img src="images/leosantana.jpeg" alt="Leo santana em cima de um trio eletrico fazendo um L com a mão" width="300" height="300">
+            
+            </article>
+            <aside class="col-lg-5 col-md-6 col-sm-12">
+                
+           <p> erat id lacinia fermentum, nulla nunc consequat ante, a feugiat sem ex vitae tellus. In non malesuada velit. Ut hendrerit facilisis elit, at pretium massa egestas in. Vivamus mattis ante sed rhoncus bibendum. Curabitur dictum sem neque, ac dignissim turpis vulputate eu. Quisque bibendum est ut facilisis consequat. Mauris auctor ligula sit amet erat aliquam, sit amet cursus nibh bibendum. Fusce ornare leo non neque scelerisque, quis molestie lacus blandit. Praesent nec interdum risus, eu rhoncus lectus. Suspendisse auctor sapien velit, et vehicula tellus viverra id. Pellentesque mollis dui eros, sit amet accumsan massa imperdiet id. Cras aliquam vel ex ut gravida. Proin eget luctus elit. Cras elementum metus sem, eget vulputate diam fermentum in.
+                Duis rhoncus auctor sem, at scelerisque magna bibendum ac. Maecenas placerat nulla pharetra enim mollis, a viverra augue pulvinar.
+                </p>
+      
+               
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a feugiat sem ex vitae tellus. In non malesuada velit. Ut hendrerit facilisis elit, at pretium massa egestas in. Vivamus mattis ante sed rhoncus bibendum. Curabitur dictum sem neque, ac dignissim turpis vulputate eu. Quisque bibendum est ut facilisis consequat. Mauris auctor ligula sit amet erat aliquam, sit amet cursus nibh bibendum. Fusce ornare leo non neque scelerisque, quis molestie lacus blandit. Praesent nec interdum risus, eu rhoncus lectus. Suspendisse auctor sapien velit, et vehicula tellus viverra id. Pellentesque mollis dui eros, sit amet accumsan massa imperdiet id. Cras aliquam vel ex ut gravida. Proin eget luctus elit. Cras elementum metus sem, eget vulputate diam fermentum in.
+                  Duis rhoncus auctor sem, at scelerisque magna bibendum ac. Maecenas placerat nulla pharetra enim mollis, a viverra augue pulvinar. 
+                </p>
+          
+       
+            
+        
+            </aside>
+        </div>
+    
+
+        <div class="row">
+            <section class="col-12 col-md-12 col-sm-12 positionbutton">
+                <button><a href="axe-primary.html">Voltar</a></button>
+            </section>
+        </div>
+
+    <div class="row footer-cor" >
+            <footer class="col-6">
+                <h3>Menu</h3>
+                <nav class="navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Estilos</a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown01">
+                                <a class="dropdown-item" href="axe-primary.html">Axé</a>
+                                <a class="dropdown-item" href="forro-primary.html">Forró</a>
+                                <a class="dropdown-item" href="rock-primary.html">Rock</a>
+                                <a class="dropdown-item" href="mpb-primary.html">Mpb</a>
+                                <a class="dropdown-item" href="samba-primary.html">Samba</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a href="quem-somos.html">Quem somos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="normas.html">Normas de uso</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contato.html">Contato</a>
+                        </li>
+                    </ul>
+                </nav>
+            </footer>
+            <div class="col align-self-end">
+
+                    <small>&copy; todos os direitos reservados</small>
+
+            </div>
+        </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+        
+        
+      
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
