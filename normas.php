42436becc0d8972e30b963348d9090c5a487a647
EsauReis <esau.santos@fatec.sp.gov.br>
2020-11-28 19:42:58 -0300

    h1s, titulo, button

--- normas.php
+++ normas.php
@@ -15,6 +15,7 @@
     <!-- O meu vem por último pq estamos reescrevendo as regras dos anteriores -->
     <link href="css/normas.css" rel="stylesheet" type="text/css" />
     <link href="css/base.css" rel="stylesheet" type="text/css" />
+
 </head>
 
 <body>
@@ -31,16 +32,14 @@
 
 
         <article class="row">
-            <h1 class="col-lg-12 col-md-12 col-sm-12 textspace">Normas</h1>
-            <img src="images/barra.png" width="100%" class="img-fluid rounded mx-auto d-block" />
+            <h1 class="offset-lg-1 col-lg-10 col-md-12 col-sm-12 textspace">Normas</h1>
+            <img src="images/barra.png" class="img-fluid rounded mx-auto d-block" />
         </article>
 
 
-
-
-
         <div class="row">
-            <section class="offset-lg-1 col-lg-5  col-md-10 col-sm-12 bordaa spacecentral ">
+
+            <section class="offset-lg-1 col-lg-5 col-md-6 col-sm-12 caixa border border-danger">
                 <h3>Norma1</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare
                     risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a
@@ -49,7 +48,7 @@
                     turpis vulputate eu. Quisque bibendum est ut facilisis consequat. Mauris auctor ligula sit amet erat
                     aliquam.</p>
             </section>
-            <section class="col-lg-5 offset-md-1 col-md-10 col-sm-12 bordaa spacecentrall">
+            <section class="col-lg-5 col-md-6 col-sm-12 caixa border border-danger">
                 <h3>Norma2</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare
                     risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a
@@ -61,7 +60,8 @@
         </div>
 
         <div class="row">
-            <section class="offset-lg-1 col-lg-5  col-md-10 col-sm-12 bordaa spacecentral ">
+
+            <section class="offset-lg-1 col-lg-5 col-md-6 col-sm-12 caixa border border-danger">
                 <h3>Norma1</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare
                     risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a
@@ -70,7 +70,7 @@
                     turpis vulputate eu. Quisque bibendum est ut facilisis consequat. Mauris auctor ligula sit amet erat
                     aliquam.</p>
             </section>
-            <section class="col-lg-5 offset-md-1 col-md-10 col-sm-12 bordaa spacecentral">
+            <section class="col-lg-5 col-md-6 col-sm-12 caixa border border-danger">
                 <h3>Norma2</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare
                     risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a
@@ -80,8 +80,10 @@
                     aliquam.</p>
             </section>
         </div>
+
         <div class="row">
-            <section class="offset-lg-1 col-lg-5  col-md-10 col-sm-12 bordaa spacecentral ">
+
+            <section class="offset-lg-1 col-lg-5 col-md-6 col-sm-12 caixa border border-danger">
                 <h3>Norma1</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare
                     risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a
@@ -90,7 +92,7 @@
                     turpis vulputate eu. Quisque bibendum est ut facilisis consequat. Mauris auctor ligula sit amet erat
                     aliquam.</p>
             </section>
-            <section class="col-lg-5 offset-md-1 col-md-10 col-sm-12 bordaa spacecentral">
+            <section class="col-lg-5 col-md-6 col-sm-12 caixa border border-danger">
                 <h3>Norma2</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis ornare
                     risus eget accumsan. Vestibulum porta, erat id lacinia fermentum, nulla nunc consequat ante, a
@@ -102,7 +104,8 @@
         </div>
 
 
-        
+
+
 
         <?php
             include 'includes/footer.php';
